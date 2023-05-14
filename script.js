9ef8ebe0e72a00bee5df19cc541eb6b794b9554f
Mario <gserna.ms@gmail.com>
2023-05-14 21:47:20 +0200

    añadiendo pantalla en negro y boton de encendido con javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function mostrar(){
+   document.getElementById('video').style.display = 'block';
+}
+
+function playvid(){
+    document.getElementById('video').play(),
+    document.getElementById('superior_dch').style.backgroundColor ='rgb(13, 243, 13)';
+}
